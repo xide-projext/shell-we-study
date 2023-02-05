54f4149d6d00c408f2b45cfe71fac3e06b1734ac
kimjihong9 <wlghddl9@gmail.com>
2023-02-06 04:48:54 +0900

    style: AddTodoModal icon

--- src/components/AddTodoModal.vue
+++ src/components/AddTodoModal.vue
@@ -7,7 +7,20 @@
             class="save_btn mb-4 bg-sky-800 hover:bg-sky-900 text-white border-none focus:outline-none"
             @click="saveTodo"
           >
-            저장
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-7 h-7"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25"
+              />
+            </svg>
           </button>
         </div>
         <div class="modal-body">
@@ -92,7 +105,16 @@
                   class="bg-white border-none focus:outline-none"
                   @click="addCoDoer"
                 >
-                  ➕
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="sky-800"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"
+                    />
+                  </svg>
                 </button>
               </div>
 
@@ -104,7 +126,20 @@
                       class="bg-white border-none focus:outline-none"
                       @click="coDoer_list.splice(index, 1)"
                     >
-                      ❌
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.8"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                        />
+                      </svg>
                     </button>
                   </li>
                 </div>
